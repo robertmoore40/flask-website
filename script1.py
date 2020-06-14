fdb77d2715dcf6d81a95e337ff881bfcda7bcbfd
robertmoore40 <robertcmoore92@gmail.com>
2020-06-14 19:40:58 -0400

    added options

--- script1.py
+++ script1.py
@@ -1,10 +1,11 @@
+#imports
 from flask import Flask, render_template
 
 app=Flask(__name__)
 
+#routes
 @app.route('/')
 def home():
-    # return render_template("home.html")
     return render_template("home.html")
 
 @app.route('/about/')
