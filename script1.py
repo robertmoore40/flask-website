69b76c0a161cd1342ef97b5842afdaa8051ca912
robertmoore40 <robertcmoore92@gmail.com>
2020-06-14 18:14:14 -0400

    changed homepage

--- script1.py
+++ script1.py
@@ -4,6 +4,10 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
+    return "Website Homepage"
+
+@app.route('/about/')
+def home():
     return "website content served here"
 
 if __name__=='__main__':
