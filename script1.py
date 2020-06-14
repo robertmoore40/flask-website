3277d546f86fde59da5982714c0b450ecb0dbbc6
robertmoore40 <robertcmoore92@gmail.com>
2020-06-14 18:25:41 -0400

    rendered template

--- script1.py
+++ script1.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Website Homepage"
+    # return render_template("home.html")
+    return render_template("home.html")
 
 @app.route('/about/')
-def home():
+def about():
     return "website content served here"
 
 if __name__=='__main__':
